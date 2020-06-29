b65114366df08e59d3091b040910ccf62e0ff5d2
javier <javier.san.mail@gmail.com>
2020-06-29 16:11:37 +0200

    added react-router and layout files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import SideBar from './components/SideBar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Routes from './routes/Routes';
+import ShoppingCart from './components/shoppingCart';
+
 const App = () => {
-    const pru = 34;
     return (
-        <div className="App-container">
-            <h1>App{pru}</h1>
+        <div className="App-container" data-test="component-app">
+            <Header />
+            <div className="App-body" data-test="App-body">
+                <SideBar />
+                <Routes />
+                <ShoppingCart />
+            </div>
+            <Footer />
         </div>
     );
 };
---
+++ client/src/components/SideBar.js
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+
+const SideBar = () => {
+    return <div>SideBar</div>;
+};
+
+export default SideBar;
