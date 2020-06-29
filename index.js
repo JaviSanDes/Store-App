74c4cb7580085f8fe727e28b3edc20cc0019cdfb
javier <javier.san.mail@gmail.com>
2020-06-29 11:41:42 +0200

    setUp server: setUp config, MongoDB, production features, and Express Routes file.

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+require('./startUp/db')();
+require('./startUp/config')();
+require('./startUp/prod')(app);
+require('./startUp/routes')(app);
+
+// statics assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
     console.log(`Listening on port ${port}...`)
