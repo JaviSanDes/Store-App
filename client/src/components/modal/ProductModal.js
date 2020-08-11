729363f2975e177ce24643bb883a2b7c29677321
javier sanchez <javier.san.mail@gmail.com>
2020-08-11 17:26:20 +0200

    added modal

---
+++ client/src/components/modal/ProductModal.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Slider from './Slider';
+import Buttons from '../Buttons';
+import { addProduct } from '../../store/actions/Products';
+
+const ProductModal = props => {
+    const {
+        id,
+        name,
+        measure,
+        description,
+        price,
+        image,
+        isVisible,
+        setVisible,
+        quantity,
+        group,
+        offer,
+    } = props;
+    const toggle = () => setVisible();
+    const dispatch = useDispatch();
+
+    return (
+        <Modal isOpen={isVisible} toggle={toggle} size="lg">
+            <ModalHeader toggle={toggle} />
+            <ModalBody>
+                <div className="modal-body">
+                    <Slider image={image} />
+                    <div className="modal-body-info">
+                        <div className="modal-body-info-nameOffer">
+                            <h1 className="modal-body-info-name">{name}</h1>
+                            {offer > 0 ? (
+                                <div className="modal-offer">{offer}%</div>
+                            ) : null}
+                        </div>
+
+                        <p>{measure}</p>
+                        <p className="modal-body-info-text">
+                            {description}
+                            <br />
+                            <br />
+                            <div>
+                                <p className="modal-tag">{group}</p>
+                            </div>
+                        </p>
+                        <div className="product-price">
+                            <p className="product-price-value">{price} €</p>
+                            {quantity > 0 ? (
+                                <Buttons id={id} quantity={quantity} />
+                            ) : (
+                                <div
+                                    role="button"
+                                    id="product-price-button"
+                                    className="avoid-modal"
+                                    onClick={() => dispatch(addProduct(id))}
+                                >
+                                    Add to Cart
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </ModalBody>
+        </Modal>
+    );
+};
+
+ProductModal.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    measure: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    isVisible: PropTypes.bool.isRequired,
+    setVisible: PropTypes.func.isRequired,
+    quantity: PropTypes.number.isRequired,
+    group: PropTypes.string.isRequired,
+    offer: PropTypes.number.isRequired,
+};
+
+export default ProductModal;
---
+++ client/src/components/modal/Slider.js
@@ -0,0 +1,111 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Slider = props => {
+    const { image } = props;
+    const [translate, setTranslate] = useState(0);
+    const [style1, setStyle1] = useState({ border: 2, color: '#019E7F' });
+    const [style2, setStyle2] = useState({ border: 1, color: '#e6e6e6' });
+    const [style3, setStyle3] = useState({ border: 1, color: '#e6e6e6' });
+
+    const sliderHandler = val => {
+        setTranslate(val);
+        switch (val) {
+            case 0:
+                setStyle1({ border: 2, color: '#019E7F' });
+                setStyle2({ border: 1, color: '#e6e6e6' });
+                setStyle3({ border: 1, color: '#e6e6e6' });
+                break;
+            case -320:
+                setStyle1({ border: 1, color: '#e6e6e6' });
+                setStyle2({ border: 2, color: '#019E7F' });
+                setStyle3({ border: 1, color: '#e6e6e6' });
+                break;
+            case -640:
+                setStyle1({ border: 1, color: '#e6e6e6' });
+                setStyle2({ border: 1, color: '#e6e6e6' });
+                setStyle3({ border: 2, color: '#019E7F' });
+                break;
+            default:
+                return val;
+        }
+        return null;
+    };
+
+    return (
+        <div className="modal-body-image">
+            <div className="modal-body-image-1">
+                <div
+                    style={{
+                        transform: `translateX(${translate}px)`,
+                        transition: 'all 0.5s',
+                    }}
+                    className="modal-body-image-slider"
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + `images/${image}`}
+                        alt="img"
+                        className="modal-body-img-1"
+                    />
+                    <img
+                        src={process.env.PUBLIC_URL + `images/${image}`}
+                        alt="img"
+                        className="modal-body-img-1"
+                    />
+                    <img
+                        src={process.env.PUBLIC_URL + `images/${image}`}
+                        alt="img"
+                        className="modal-body-img-1"
+                    />
+                </div>
+            </div>
+
+            <div className="modal-body-image-2">
+                <div
+                    style={{
+                        border: `${style1.border}px solid ${style1.color}`,
+                    }}
+                    onClick={() => sliderHandler(0)}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + `images/${image}`}
+                        alt="img"
+                        className="modal-body-img-2"
+                    />
+                </div>
+                <div
+                    style={{
+                        border: `${style2.border}px solid ${style2.color}`,
+                    }}
+                    onClick={() => sliderHandler(-320)}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + `images/${image}`}
+                        alt="img"
+                        className="modal-body-img-2"
+                    />
+                </div>
+                <div
+                    style={{
+                        border: `${style3.border}px solid ${style3.color}`,
+                    }}
+                    onClick={() => sliderHandler(-640)}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + `images/${image}`}
+                        alt="img"
+                        className="modal-body-img-2"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Slider.propTypes = {
+    image: PropTypes.string.isRequired,
+};
+
+export default Slider;
