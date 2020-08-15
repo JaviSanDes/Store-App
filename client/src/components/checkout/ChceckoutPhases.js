29ab8aaeb7215e74a4ed51dbcea357043751288a
javier sanchez <javier.san.mail@gmail.com>
2020-08-15 16:51:52 +0200

    [styled] checkoutphases circles

--- client/src/components/checkout/ChceckoutPhases.js
+++ client/src/components/checkout/ChceckoutPhases.js
@@ -6,21 +6,21 @@ const CheckoutPhases = () => {
             <div className="checkout-phases-line"></div>
             <div className="checkout-phases-circles">
                 <div className="checkout-phase">
-                    <div className="checkout-circle"></div>
-                    <p>phase 1</p>
+                    <div className="checkout-circle">1</div>
+                    <p>Your Cart</p>
                 </div>
 
                 <div className="checkout-phase">
-                    <div className="checkout-circle"></div>
-                    <p>phase 2</p>
+                    <div className="checkout-circle">2</div>
+                    <p>Shipping</p>
                 </div>
                 <div className="checkout-phase">
-                    <div className="checkout-circle"></div>
-                    <p>phase 3</p>
+                    <div className="checkout-circle">3</div>
+                    <p>Payment</p>
                 </div>
                 <div className="checkout-phase">
-                    <div className="checkout-circle"></div>
-                    <p>phase 4</p>
+                    <div className="checkout-circle">4</div>
+                    <p>Summary</p>
                 </div>
             </div>
         </div>
