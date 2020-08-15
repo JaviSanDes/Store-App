8ada2ba578d79d62b16d81ba7b1f76c2232fea40
javier sanchez <javier.san.mail@gmail.com>
2020-08-15 19:10:01 +0200

    [fixed] file name checkoutPhases component

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import CehckoutPhases from '../components/checkout/ChceckoutPhases';
+import CheckoutPhases from '../components/checkout/CheckoutPhases';
 import Order from '../components/checkout/Order';
 
 const Checkout = () => {
     return (
         <div className="checkout-container">
             <h1>Checkout</h1>
-            <CehckoutPhases />
+            <CheckoutPhases />
             <Order />
         </div>
     );
