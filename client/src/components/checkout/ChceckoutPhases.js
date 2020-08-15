22b3315446ee3cf6dba568227cf56fd14e232509
javier sanchez <javier.san.mail@gmail.com>
2020-08-15 14:26:10 +0200

    [checkout] starting order component

---
+++ client/src/components/checkout/ChceckoutPhases.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CheckoutPhases = () => {
+    return (
+        <div>
+            <div className="checkout-circle"></div>
+            <div className="checkout-circle"></div>
+            <div className="checkout-circle"></div>
+            <div className="checkout-circle"></div>
+        </div>
+    );
+};
+
+export default CheckoutPhases;
--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import CehckoutPhases from '../components/checkout/ChceckoutPhases';
+import Order from '../components/checkout/Order';
 
 const Checkout = () => {
     return (
         <div className="checkout-container">
             <h1>Checkout</h1>
+            <CehckoutPhases />
+            <Order />
         </div>
     );
 };
