577408cc4b1748ca32e9a1d288cb312b3bb8ca2b
javier sanchez <javier.san.mail@gmail.com>
2020-08-15 16:40:23 +0200

    [updating] checkoutPahses component

--- client/src/components/checkout/ChceckoutPhases.js
+++ client/src/components/checkout/ChceckoutPhases.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 const CheckoutPhases = () => {
     return (
-        <div>
-            <div className="checkout-circle"></div>
-            <div className="checkout-circle"></div>
-            <div className="checkout-circle"></div>
-            <div className="checkout-circle"></div>
+        <div className="checkout-phases-container">
+            <div className="checkout-phases-line"></div>
+            <div className="checkout-phases-circles">
+                <div className="checkout-phase">
+                    <div className="checkout-circle"></div>
+                    <p>phase 1</p>
+                </div>
+
+                <div className="checkout-phase">
+                    <div className="checkout-circle"></div>
+                    <p>phase 2</p>
+                </div>
+                <div className="checkout-phase">
+                    <div className="checkout-circle"></div>
+                    <p>phase 3</p>
+                </div>
+                <div className="checkout-phase">
+                    <div className="checkout-circle"></div>
+                    <p>phase 4</p>
+                </div>
+            </div>
         </div>
     );
 };
