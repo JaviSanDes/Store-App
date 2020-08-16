4fd207f1f2c415606fd8bc5dbe51bad11836b289
javier sanchez <javier.san.mail@gmail.com>
2020-08-16 14:01:42 +0200

    [creating] order component

--- client/src/components/checkout/Order.js
+++ client/src/components/checkout/Order.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Product2 from '../shoppingCart/Product2';
 
 const Order = () => {
+    const productsOrdered = useSelector(state => state.products.products);
+    const order = useSelector(state => state.products.order);
+
+    const products = [];
+    order.map(id => {
+        productsOrdered.map(product => {
+            if (product._id === id && product.quantity > 0) {
+                products.push(
+                    <Product2
+                        key={product._id}
+                        id={product._id}
+                        name={product.name}
+                        group={product.group}
+                        description={product.description}
+                        price={product.price}
+                        image={product.image}
+                        quantity={product.quantity}
+                        measure={product.measure}
+                    />
+                );
+            }
+            return null;
+        });
+        return null;
+    });
     return (
-        <div>
-            <h1>Your cart</h1>
+        <div className="order-container">
+            <div className="order-products">{products}</div>
+            <div className="order-info">
+                <h4>Your Bags are Ready to Check Out!</h4>
+                <p>
+                    Discover goods you will love from brands that inspire. The
+                    easiest way to open your own online store. Discover amazing
+                    stuff or open your own store for free!
+                </p>
+                <p>
+                    Below is a sample page for your cart, Created using pages
+                    design UI Elementes
+                </p>
+            </div>
         </div>
     );
 };
--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import CheckoutPhases from '../components/checkout/CheckoutPhases';
+import ShoppingCart from '../components/checkout/CheckoutPhases';
 import Order from '../components/checkout/Order';
 
 const Checkout = () => {
     return (
         <div className="checkout-container">
-            <h1>Checkout</h1>
-            <CheckoutPhases />
+            <ShoppingCart />
             <Order />
         </div>
     );
