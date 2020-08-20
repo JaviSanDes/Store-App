569844b066dc3090b8ec4c2d909bae15f0cb1079
javier sanchez <javier.san.mail@gmail.com>
2020-08-20 18:31:36 +0200

    [added] payment component features

--- client/src/components/checkout/Payment.js
+++ client/src/components/checkout/Payment.js
@@ -4,18 +4,25 @@ const Payment = () => {
     return (
         <div className="payment-container">
             <h1>payment</h1>
+            <h4>Price: 345€</h4>
             <p>Payment Method</p>
             <div className="payment-method">
                 <div>Card</div>
-                <div>Paypal</div>
+                <div className="payment-method-2">Paypal</div>
                 <div>Digital Wallet</div>
             </div>
             <p>Card number</p>
             <input></input>
-            <p>Expiration</p>
-            <input></input>
-            <p>CVC Code</p>
-            <input></input>
+            <div className="payment-card-details">
+                <div>
+                    <p>Expiration</p>
+                    <input></input>
+                </div>
+                <div>
+                    <p>CVC Code</p>
+                    <input></input>
+                </div>
+            </div>
         </div>
     );
 };
