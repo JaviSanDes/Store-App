711525dc3dbf911fed19b653324bd2fb5d562a86
javier sanchez <javier.san.mail@gmail.com>
2020-08-24 17:35:14 +0200

    payment form

--- client/src/components/checkout/Payment.js
+++ client/src/components/checkout/Payment.js
@@ -4,7 +4,6 @@ import { Input } from 'reactstrap';
 const Payment = () => {
     return (
         <div className="payment-container">
-            <h1>payment</h1>
             <h4>Price: 345€</h4>
             <p>Payment Method</p>
             <div className="payment-method">
@@ -12,6 +11,27 @@ const Payment = () => {
                 <div className="payment-method-2">Paypal</div>
                 <div>Digital Wallet</div>
             </div>
+            <div className="payment-card-details">
+                <div>
+                    <p>Personal Details</p>
+                    <Input
+                        type="text"
+                        name="password"
+                        id="examplePassword"
+                        placeholder="Enter full name"
+                    />
+                </div>
+                <div>
+                    <p>df</p>
+                    <Input
+                        type="text"
+                        name="password"
+                        id="examplePassword"
+                        placeholder="Enter full name"
+                    />
+                </div>
+            </div>
+
             <p>Card number</p>
             <Input
                 type="text"
