d72b3512b3850b1208ac57de7cd379d39619f877
javier sanchez <javier.san.mail@gmail.com>
2020-08-24 17:37:17 +0200

    created summary on checkout component

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -2,13 +2,14 @@ import React from 'react';
 import CheckoutPhases from '../components/checkout/CheckoutPhases';
 // import Order from '../components/checkout/Order';
 // import Shipping from '../components/checkout/Shipping';
-import Payment from '../components/checkout/Payment';
+// import Payment from '../components/checkout/Payment';
+import Summary from '../components/checkout/Summary';
 
 const Checkout = () => {
     return (
         <div className="checkout-container">
             <CheckoutPhases />
-            <Payment />
+            <Summary />
             <button className="checkout-nextButton" type="button">
                 Next
             </button>
