7d32141c7ee65c3dec166723aefc8b3e73aa72cf
javier sanchez <javier.san.mail@gmail.com>
2020-08-26 17:06:56 +0200

    added height on checkout container

--- client/src/components/checkout/Shipping.jsx
+++ client/src/components/checkout/Shipping.jsx
@@ -3,39 +3,10 @@ import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Shipping = () => {
     return (
-        <div className="shipping-container">
-            <Form>
+        <Form className="shipping-container">
+            <FormGroup>
                 <FormGroup>
-                    <FormGroup>
-                        <Label for="examplePassword">Full Name</Label>
-                        <Input
-                            type="text"
-                            name="password"
-                            id="examplePassword"
-                            placeholder="Enter full name"
-                        />
-                    </FormGroup>
-                    <Label for="exampleSelect">Contry</Label>
-                    <Input type="select" name="select">
-                        <option>Spain</option>
-                        <option>United Kingdom</option>
-                        <option>France</option>
-                        <option>Germany</option>
-                        <option>Italy</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Address</Label>
-                    <Input
-                        type="text"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="Current address..."
-                    />
-                </FormGroup>
-
-                <FormGroup>
-                    <Label for="examplePassword">ZIP Code</Label>
+                    <Label for="examplePassword">Full Name</Label>
                     <Input
                         type="text"
                         name="password"
@@ -43,18 +14,45 @@ const Shipping = () => {
                         placeholder="Enter full name"
                     />
                 </FormGroup>
+                <Label for="exampleSelect">Contry</Label>
+                <Input type="select" name="select">
+                    <option>Spain</option>
+                    <option>United Kingdom</option>
+                    <option>France</option>
+                    <option>Germany</option>
+                    <option>Italy</option>
+                </Input>
+            </FormGroup>
+            <FormGroup>
+                <Label for="examplePassword">Address</Label>
+                <Input
+                    type="text"
+                    name="password"
+                    id="examplePassword"
+                    placeholder="Current address..."
+                />
+            </FormGroup>
 
-                <FormGroup>
-                    <Label for="examplePassword">Phone Number</Label>
-                    <Input
-                        type="text"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="Current address..."
-                    />
-                </FormGroup>
-            </Form>
-        </div>
+            <FormGroup>
+                <Label for="examplePassword">ZIP Code</Label>
+                <Input
+                    type="text"
+                    name="password"
+                    id="examplePassword"
+                    placeholder="Enter full name"
+                />
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="examplePassword">Phone Number</Label>
+                <Input
+                    type="text"
+                    name="password"
+                    id="examplePassword"
+                    placeholder="Current address..."
+                />
+            </FormGroup>
+        </Form>
     );
 };
 
