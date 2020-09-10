d87c2b9f48c000a696b4ac38a32180c263044990
javier sanchez <javier.san.mail@gmail.com>
2020-09-10 16:27:39 +0200

    [added] box style on settings page

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Settings = () => {
     return (
-        <div>
+        <div className="settings-container">
             <h1>Settings</h1>
             <h1>Settings</h1>
             <h1>Settings</h1>
