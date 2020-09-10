27e1e7eea60f2ea38874cbdd7df0b3b37fd99fb2
javier sanchez <javier.san.mail@gmail.com>
2020-09-10 16:41:03 +0200

    [copy] shipping form into settings.js

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Settings = () => {
     return (
         <div className="settings-container">
-            <h1>Settings</h1>
-            <h1>Settings</h1>
-            <h1>Settings</h1>
-            <h1>Settings</h1>
-            <h1>Settings</h1>
+            <Form className="shipping-container">
+                <FormGroup>
+                    <FormGroup>
+                        <Label for="examplePassword">Full Name</Label>
+                        <Input
+                            type="text"
+                            name="password"
+                            id="examplePassword"
+                            placeholder="Enter full name"
+                            minlength="8"
+                            required
+                        />
+                    </FormGroup>
+                    <Label for="exampleSelect">Contry</Label>
+                    <Input type="select" name="select">
+                        <option>Spain</option>
+                        <option>United Kingdom</option>
+                        <option>France</option>
+                        <option>Germany</option>
+                        <option>Italy</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="examplePassword">Address</Label>
+                    <Input
+                        type="text"
+                        name="password"
+                        id="examplePassword"
+                        placeholder="Current address..."
+                        minlength="8"
+                        required
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="examplePassword">ZIP Code</Label>
+                    <Input
+                        type="text"
+                        name="password"
+                        id="examplePassword"
+                        placeholder="Enter full name"
+                        minLength="5"
+                        maxLength="5"
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="examplePassword">Phone Number</Label>
+                    <Input
+                        type="number"
+                        name="password"
+                        id="examplePassword"
+                        placeholder="Current address..."
+                    />
+                </FormGroup>
+                <input type="submit" value="Submit"></input>
+            </Form>
         </div>
     );
 };
