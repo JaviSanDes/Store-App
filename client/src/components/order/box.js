bd54ab433759d7e820bb0c851387b9f297e30581
javier sanchez <javier.san.mail@gmail.com>
2020-09-25 16:45:03 +0200

    move all order box logic into box component

--- client/src/components/order/box.js
+++ client/src/components/order/box.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Box = () => {
     return (
-        <div>
-            <h1>Box</h1>
+        <div className="orders-orderBox">
+            <div className="orders-orderBox-header">
+                <p>Order 1</p>
+                <div className="orders-orderBox-header-status">
+                    <p>Order Delivered</p>
+                </div>
+            </div>
+            <div className="orders-orderInfo">
+                <div>
+                    <p>Order Date:</p>
+                    <p>Delivery Time:</p>
+                    <p className="orders-orderInfo-prcie">Total Price:</p>
+                </div>
+                <div className="orders-orderInfo-values">
+                    <p>7th April 2019</p>
+                    <p>13th April</p>
+                    <p className="orders-orderInfo-values-price">$249.7</p>
+                </div>
+            </div>
+            <button className="orders-orderBox-button" type="button">
+                View order
+            </button>
         </div>
     );
 };
--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -1,40 +1,17 @@
 import React from 'react';
+import Box from '../components/order/box';
 
 const Orders = () => {
     return (
         <div className="orders-container">
             <div className="orders-ordersList">
                 <h4>My Orders</h4>
-                <div className="orders-orderBox">
-                    <div className="orders-orderBox-header">
-                        <p>Order 1</p>
-                        <div className="orders-orderBox-header-status">
-                            <p>Order Delivered</p>
-                        </div>
-                    </div>
-                    <div className="orders-orderInfo">
-                        <div>
-                            <p>Order Date:</p>
-                            <p>Delivery Time:</p>
-                            <p className="orders-orderInfo-prcie">
-                                Total Price:
-                            </p>
-                        </div>
-                        <div className="orders-orderInfo-values">
-                            <p>7th April 2019</p>
-                            <p>13th April</p>
-                            <p className="orders-orderInfo-values-price">
-                                $249.7
-                            </p>
-                        </div>
-                    </div>
-                    <button className="orders-orderBox-button" type="button">
-                        View order
-                    </button>
-                </div>
-                <div className="orders-orderBox"></div>
-                <div className="orders-orderBox"></div>
-                <div className="orders-orderBox"></div>
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
             </div>
             <div className="orders-orderDetails">
                 <h4>Order Details</h4>
