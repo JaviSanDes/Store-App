c9701a1b92c4ef30aa6366a235c766cd1ed1a85c
javier sanchez <javier.san.mail@gmail.com>
2020-10-04 11:50:19 +0200

    ·

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -27,7 +27,6 @@ const Orders = () => {
                                 X
                             </button>
                         </div>
-
                         <div className="orders-delivery-info">
                             <div className="orders-delivery-info-1">
                                 <div>
@@ -57,34 +56,36 @@ const Orders = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="orders-steps">
-                            <div className="orders-circle orders-ticked">
-                                <img
-                                    src={
-                                        process.env.PUBLIC_URL +
-                                        'images/tick2.png'
-                                    }
-                                    className="orders-tick"
-                                    alt="img"
-                                />
+                        <div className="orders-deliveryStatus">
+                            <div className="orders-steps">
+                                <div className="orders-circle orders-ticked">
+                                    <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            'images/tick2.png'
+                                        }
+                                        className="orders-tick"
+                                        alt="img"
+                                    />
+                                </div>
+                                <div className="orders-circle orders-ticked">
+                                    <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            'images/tick2.png'
+                                        }
+                                        className="orders-tick"
+                                        alt="img"
+                                    />
+                                </div>
+                                <div className="orders-circle">3</div>
                             </div>
-                            <div className="orders-circle orders-ticked">
-                                <img
-                                    src={
-                                        process.env.PUBLIC_URL +
-                                        'images/tick2.png'
-                                    }
-                                    className="orders-tick"
-                                    alt="img"
-                                />
+                            <Progress value={50} className="orders-progress" />
+                            <div className="orders-steps">
+                                <p>Order Recived</p>
+                                <p>Order On The Way</p>
+                                <p>Order Delivered</p>
                             </div>
-                            <div className="orders-circle">3</div>
-                        </div>
-                        <Progress value={50} className="orders-progress" />
-                        <div className="orders-steps">
-                            <p>Order Recived</p>
-                            <p>Order On The Way</p>
-                            <p>Order Delivered</p>
                         </div>
                     </div>
 
