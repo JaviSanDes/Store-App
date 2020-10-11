af7a5e237ac6ccad6d2981a97180254bba3ae6c0
javier sanchez <javier.san.mail@gmail.com>
2020-10-11 13:22:50 +0200

    set height of orderItems on orders page

--- client/src/components/orders/OrderItems.js
+++ client/src/components/orders/OrderItems.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import Product from './Product';
 
 const OrderItems = () => {
     const productsOrderItemsed = useSelector(state => state.products.products);
     const orderItems = useSelector(state => state.products.order);
+
+    const handleUserKeyPress = useCallback(() => {
+        console.log('ENTRAAAA');
+        setTimeout(() => {
+            const down = document.getElementById('prueba3');
+            const header = document.getElementById('prueba4');
+            const productList = document.getElementById('prueba5');
+            const result = down.offsetHeight - header.offsetHeight;
+            productList.style.height = `${result}px`;
+        }, 500);
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleUserKeyPress);
+        handleUserKeyPress();
+        return () => {
+            window.removeEventListener('resize', handleUserKeyPress);
+        };
+    }, []);
 
     const products = [];
     orderItems.map(id => {
@@ -30,15 +49,16 @@ const OrderItems = () => {
     });
     return (
         <div className="orderItems-container">
-            <div className="orderItems-products-header">
+            <div className="orderItems-products-header" id="prueba4">
                 <p className="orderItems-products-header-description">
                     Description
                 </p>
                 <p className="orderItems-products-header-qty">Qty</p>
                 <p className="orderItems-products-header-total">Price</p>
             </div>
-
-            <div className="orderItems-products-list">{products}</div>
+            <div className="orderItems-products-list" id="prueba5">
+                {products}
+            </div>
         </div>
     );
 };
