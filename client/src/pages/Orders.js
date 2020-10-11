abf279025a3e5fd4ff4d640e7ef341849edf2369
javier sanchez <javier.san.mail@gmail.com>
2020-10-11 11:57:23 +0200

    added height calculation of orders page component on resize

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Progress } from 'reactstrap';
 import Box from '../components/orders/box';
 import OrderItems from '../components/orders/OrderItems';
@@ -6,6 +6,30 @@ import OrderItems from '../components/orders/OrderItems';
 const Orders = () => {
     const [isVisible, setIsVisible] = useState(true);
     const boxToggle = () => setIsVisible(Visible => !Visible);
+
+    /* useEffect(() => {
+        const box = document.getElementById("prueba");
+        const up = document.getElementById("prueba2");
+        const down = document.getElementById("prueba3");
+        const result = box.offsetHeight - up.offsetHeight;
+        down.style.height = `${result - 40}px`;
+    }, []); */
+
+    const handleUserKeyPress = useCallback(() => {
+        const box = document.getElementById('prueba');
+        const up = document.getElementById('prueba2');
+        const down = document.getElementById('prueba3');
+        const result = box.offsetHeight - up.offsetHeight;
+        down.style.height = `${result - 40}px`;
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleUserKeyPress);
+        handleUserKeyPress();
+        return () => {
+            window.removeEventListener('resize', handleUserKeyPress);
+        };
+    }, [handleUserKeyPress]);
 
     return (
         <div className="orders-container">
@@ -19,8 +43,8 @@ const Orders = () => {
                 <Box />
             </div>
             {isVisible && (
-                <div className="orders-orderDetails">
-                    <div className="orders-orderDetails-up">
+                <div className="orders-orderDetails" id="prueba">
+                    <div className="orders-orderDetails-up" id="prueba2">
                         <div className="orders-orderDetails-title">
                             <h4>Order Details</h4>
                             <button onClick={boxToggle} type="button">
@@ -89,7 +113,7 @@ const Orders = () => {
                         </div>
                     </div>
 
-                    <div className="orders-orderDetails-down">
+                    <div className="orders-orderDetails-down" id="prueba3">
                         <OrderItems />
                     </div>
                 </div>
