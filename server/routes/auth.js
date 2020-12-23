84e67cf97924f701c9687928bcb9ee8e918e29ec
javier sanchez <javier.san.mail@gmail.com>
2020-12-23 13:16:48 +0100

    added userInfo endpoint to get user info on the front

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -55,6 +55,20 @@ router.post('/signUp', async (req, res) => {
   res.send({_id, firstName, lastName, email, token});
 });
 
+router.post('/userInfo', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid password.');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid password.');
+
+  const { firstName, lastName, email } = user;
+  res.send({firstName, lastName, email});
+});
+
 function validate(req) {
   const schema = Joi.object({
     email: Joi.string().min(5).max(255).required().email(),
