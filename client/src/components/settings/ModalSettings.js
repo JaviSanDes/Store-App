c08f5c8775da0edbdc5e1e1b8177b3b9213e2fdd
javier sanchez <javier.san.mail@gmail.com>
2021-01-04 19:53:52 +0100

    refactor modal on settings component into separate component

---
+++ client/src/components/settings/ModalSettings.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Modal, ModalBody, ModalFooter } from 'reactstrap';
+
+const ModalSettings = props => {
+    const { modal, deleteAccModal, modalHandler, deleteAccountHandler } = props;
+    return (
+        <Modal isOpen={modal}>
+            <ModalBody className="summary-modal-body">
+                <div>
+                    {deleteAccModal ? (
+                        <p>Are your sore you want to delete the account</p>
+                    ) : (
+                        <p>Your data has been updated successfully</p>
+                    )}
+                </div>
+                <div>
+                    <svg
+                        className="checkmark"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 52 52"
+                    >
+                        <circle
+                            className="checkmark__circle"
+                            cx="26"
+                            cy="26"
+                            r="25"
+                            fill="none"
+                        />
+                        <path
+                            className="checkmark__check"
+                            fill="none"
+                            d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                        />
+                    </svg>
+                </div>
+            </ModalBody>
+            <ModalFooter className="summary-modal-footer">
+                {!deleteAccModal ? (
+                    <div>
+                        <button type="button" onClick={modalHandler}>
+                            Go Home
+                        </button>
+                    </div>
+                ) : (
+                    <button type="button" onClick={deleteAccountHandler}>
+                        Delete Account
+                    </button>
+                )}
+            </ModalFooter>
+        </Modal>
+    );
+};
+
+ModalSettings.propTypes = {
+    modal: PropTypes.string.isRequired,
+    deleteAccModal: PropTypes.string.isRequired,
+    modalHandler: PropTypes.string.isRequired,
+    deleteAccountHandler: PropTypes.string.isRequired,
+};
+
+export default ModalSettings;
--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 import React, { useState, useEffect } from 'react';
-import { Modal, ModalBody, ModalFooter } from 'reactstrap';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Auth from '../components/settings/Auth';
 import SettingsForm from '../components/settings/SettingsForm';
+import ModalSettings from '../components/settings/ModalSettings';
 
 const Settings = () => {
     const [password, setPassword] = useState('');
@@ -203,50 +203,12 @@ const Settings = () => {
                     switchModal={switchModal}
                 />
             )}
-            <Modal isOpen={modal}>
-                <ModalBody className="summary-modal-body">
-                    <div>
-                        {deleteAccModal ? (
-                            <p>Are your sore you want to delete the account</p>
-                        ) : (
-                            <p>Your data has been updated successfully</p>
-                        )}
-                    </div>
-                    <div>
-                        <svg
-                            className="checkmark"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 52 52"
-                        >
-                            <circle
-                                className="checkmark__circle"
-                                cx="26"
-                                cy="26"
-                                r="25"
-                                fill="none"
-                            />
-                            <path
-                                className="checkmark__check"
-                                fill="none"
-                                d="M14.1 27.2l7.1 7.2 16.7-16.8"
-                            />
-                        </svg>
-                    </div>
-                </ModalBody>
-                <ModalFooter className="summary-modal-footer">
-                    {!deleteAccModal ? (
-                        <div>
-                            <button type="button" onClick={modalHandler}>
-                                Go Home
-                            </button>
-                        </div>
-                    ) : (
-                        <button type="button" onClick={deleteAccountHandler}>
-                            Delete Account
-                        </button>
-                    )}
-                </ModalFooter>
-            </Modal>
+            <ModalSettings
+                modal={modal}
+                deleteAccModal={deleteAccModal}
+                modalHandler={modalHandler}
+                deleteAccountHandler={deleteAccountHandler}
+            />
         </div>
     );
 };
