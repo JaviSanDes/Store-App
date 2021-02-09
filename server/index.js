56d9814ff5092d9fa6838db2334cce0c086a4c3d
javier sanchez <javier.san.mail@gmail.com>
2021-02-09 19:35:53 +0100

    removed serve static file

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 
 app.use(cors());
 app.options('*', cors());
-app.use(express.static(path.join(__dirname, 'build')));
 
 require('./startUp/db')();
 require('./startUp/config')();
@@ -15,15 +14,11 @@ require('./startup/validation')();
 
 // statics assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
 }
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
