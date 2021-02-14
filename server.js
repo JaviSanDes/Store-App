3794f79368c87d96f617668115b04ac29cce9100
javier sanchez <javier.san.mail@gmail.com>
2021-02-14 16:15:45 +0100

    .

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ require('./server/startup/validation')();
 
 // statics assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
